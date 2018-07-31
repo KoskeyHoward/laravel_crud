8b6fabe0e628552f6ff03c273eb02a91fff507ab
Le-Yo <koskeyhoward@gmail.com>
2018-07-31 13:04:43 +0300

    added crud views and routes to the project

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -1,10 +1,59 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Ticket;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
     //
+
+public function create()
+{
+   return view('user.create');
+}
+
+public function index()
+    {
+        $tickets = Ticket::where('user_id', auth()->user()->id)->get();
+        
+        return view('user.index',compact('tickets'));
+    }
+
+public function store(Request $request)
+    {
+        $ticket = new Ticket();
+        $data = $this->validate($request, [
+            'description'=>'required',
+            'title'=> 'required'
+        ]);
+       
+        $ticket->saveTicket($data);
+      
+        return redirect('/home')->with('success', 'New support ticket has been created! Wait sometime to get resolved');
+    }
+public function edit($id)
+    {
+        $ticket = Ticket::where('user_id', auth()->user()->id)
+                        ->where('id', $id)
+                        ->first();
+
+        return view('user.edit', compact('ticket', 'id'));
+    }
+   
+ public function update(Request $request, $id)
+    {
+        $ticket = new Ticket();
+        $data = $this->validate($request, [
+            'description'=>'required',
+            'title'=> 'required'
+        ]);
+        $data['id'] = $id;
+        $ticket->updateTicket($data);
+
+        return redirect('/home')->with('success', 'New support ticket has been updated!!');
+    }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//tickets routes
+Route::get('/create/ticket','TicketController@create');
+Route::post('/create/ticket','TicketController@store');
+Route::get('/tickets', 'TicketController@index');
+Route::get('/edit/ticket/{id}','TicketController@edit');
+Route::post('/edit/ticket/{id}','TicketController@update');
+Route::delete('/delete/ticket/{id}','TicketController@destroy');
+
